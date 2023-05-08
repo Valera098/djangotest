9a66016c3cd41fa4e94c05b7b7bc46688c526c9b
Valera098 <Valera098Main@mail.ru>
2023-05-09 02:30:55 +0300

    third lesson made

--- coolsite/women/views.py
+++ coolsite/women/views.py
@@ -1,8 +1,19 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound
+from django.shortcuts import render, redirect
+
 
 def index(request):
     return HttpResponse("Страница приложения women")
 
-def categories(request):
-    return HttpResponse("<h1>Статьи по категориям:</h1>")
+def categories(request, catid):
+    return HttpResponse(f"<h1>Статьи по категориям:</h1><p>{catid}</p>")
+
+
+def pageNotFound(requst, exception):
+    return HttpResponseNotFound('<h1>404</h1><p>Друг, страница не найдена, шел бы отсюда.</p>')
+
+def archive(request, year):
+    if int(year) > 2020:
+        return redirect('home', permanent=False)
+
+    return HttpResponse(f"<h1>Архив по годам:</h1><p>{year}</p>")
