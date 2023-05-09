120b68e7508c48e9b9a776afd315980363430c23
Valera098 <Valera098Main@mail.ru>
2023-05-09 19:18:50 +0300

    lesson fifth finished, little bit of sixth started

--- coolsite/women/views.py
+++ coolsite/women/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, redirect
 
 
 def index(request):
-    return HttpResponse("Страница приложения women")
+    return render(request, 'women/index.html')
 
 def categories(request, catid):
     return HttpResponse(f"<h1>Статьи по категориям:</h1><p>{catid}</p>")
