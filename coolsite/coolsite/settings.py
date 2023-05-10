6c8a43b546b409a887e874b19e7f8f7046c6f03e
Valera098 <Valera098Main@mail.ru>
2023-05-10 18:28:57 +0300

    finished tenth lesson

--- coolsite/coolsite/settings.py
+++ coolsite/coolsite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_uvoz2r_+x-uh6q(92__2dp4z9pntcp!_nwmr44vh875^2^%=v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
@@ -104,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'UTC'
 
--- coolsite/women/admin.py
+++ coolsite/women/admin.py
@@ -1,2 +1,18 @@
 from django.contrib import admin
 
+from .models import *
+
+class WomenAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'time_created', 'photo', 'is_published')
+    list_display_links = ('id', 'title')
+    search_fields = ('title', 'contents')
+    list_editable = ('is_published',)
+    list_filter = ('is_published', 'time_created')
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    list_display_links = ('id', 'name')
+    search_fields = ('name',)
+
+admin.site.register(Women, WomenAdmin)
+admin.site.register(Category, CategoryAdmin)
